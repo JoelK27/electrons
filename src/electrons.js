e815c595c03ab87ca673d24ace412909eec6e9ce
Elias Zessner <elias.zessner@gmail.com>
2024-05-25 16:40:47 +0200

    Working test for test case 4 by Elias

--- src/electrons.js
+++ src/electrons.js
@@ -2,22 +2,11 @@ function Electrons() {}
 
 Electrons.prototype.add = function(string_numbers) {
     let numbers = string_numbers.split(',').map(Number);
-    let sum = numbers[0];
-
-    if (numbers.length > 5) {
-        for (let i = numbers.length - 1; i >= 0; i--) {
-            if (numbers[i] % 2 === 1) {
-                sum -= numbers[i];
-            }
+    let sum = numbers.reduce((total, num) => {
+        if (num % 2 !== 0) {
+            total += num - 1;
         }
-    } else {
-        sum = numbers.reduce((total, num) => {
-            if (num % 2 === 0) {
-                total += num;
-            }
-            return total;
-        }, 0);
-    }
-
+        return total;
+    }, 0);
     return sum.toString();
 }
