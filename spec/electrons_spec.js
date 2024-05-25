690f166cc9eb74adef8b2555c7930bd86c9841e5
Elias Zessner <elias.zessner@gmail.com>
2024-05-25 16:11:35 +0200

    Faling test for test case 2 by Elias

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -7,4 +7,8 @@ describe('Electrons around the core', function() {
         expect(electron.add('1, 2, 3, 4, 5')).toEqual('6');
     });
 
+    it('should return the number 6 for the dices "2, 2, 3, 3" rolled', function() {
+        expect(electron.add('2, 2, 3, 3')).toEqual('4');
+    });
+
 });
