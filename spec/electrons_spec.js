5d3df4a1fbf7af4a8c4a90ad6ea4a487ae497685
Joel Kakkattu <if23b095@wien-technikum.at>
2024-05-25 16:01:44 +0200

    Failing test for test case 1 by Joel

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -2,4 +2,9 @@ describe('Electrons around the core', function() {
     beforeEach(function() {
         electron = new Electrons();
     });
+    
+    it('should return the number 6 for the dices "1, 2, 3, 4, 5" rolled', function() {
+        expect(electrons.add('1, 2, 3, 4, 5')).toEqual('6');
+    });
+
 });
