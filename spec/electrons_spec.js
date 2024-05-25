a03bc13757c73755fa918e756b038cdcd8bcf58a
Joel Kakkattu <if23b095@wien-technikum.at>
2024-05-25 15:59:24 +0200

    Initial commit

---
+++ spec/electrons_spec.js
@@ -0,0 +1,5 @@
+describe('Electrons around the core', function() {
+    beforeEach(function() {
+        electron = new Electrons();
+    });
+});
---
+++ src/electrons.js
@@ -0,0 +1,6 @@
+function Electrons() {}
+
+Electrons.prototype.add = function(string_numbers) {
+
+}
+
